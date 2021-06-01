7050be4c959f948131d03f5ce82cd54cc34b0442
Gergő Tisza <tgr.huwiki@gmail.com>
2021-06-01 15:46:40 +0200

    Initial commit
    Factored out from I877909d1113ab93b4b8a.
    
    Bug: T284732
    Change-Id: Ia3affe518f7d97cf8f4c860c03c2985a6e339d45

---
+++ tests/phpunit/NormalizedExceptionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Wikimedia\NormalizedException;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Wikimedia\NormalizedException\NormalizedException;
+
+/**
+ * @covers \Wikimedia\NormalizedException\NormalizedException
+ */
+class NormalizedExceptionTest extends TestCase {
+
+	public function testConstruct() {
+		$previousException = new Exception();
+		$exception = new NormalizedException( 'foo', [ 'bar' => 'baz' ], 1, $previousException );
+		$this->assertSame( 'foo', $exception->getNormalizedMessage() );
+		$this->assertSame( [ 'bar' => 'baz' ], $exception->getMessageContext() );
+		$this->assertSame( 1, $exception->getCode() );
+		$this->assertSame( $previousException, $exception->getPrevious() );
+	}
+
+	/** @dataProvider provideMessage */
+	public function testMessage( $normalizedMessage, $context, $expectedMessage ) {
+		$exception = new NormalizedException( $normalizedMessage, $context );
+		$this->assertSame( $expectedMessage, $exception->getMessage() );
+	}
+
+	public function provideMessage() {
+		return [
+			[ 'foo', [ 'bar' => 'baz' ], 'foo' ],
+			[ '{foo}', [ 'bar' => 'baz' ], '{foo}' ],
+			[ 'bar', [ 'bar' => 'baz' ], 'bar' ],
+			[ '{bar}', [ 'bar' => 'baz' ], 'baz' ],
+			[ 'a {b} c {d} e', [ 'b' => 'x', 'd' => 'y' ], 'a x c y e' ],
+			[ 'a {b} c {d} e', [ 'd' => 'y', 'b' => 'x' ], 'a x c y e' ],
+			[ '{v}', [ 'v' => 1 ], '1' ],
+			[ '{v}', [ 'v' => 1.0 ], '1' ],
+			[ '{v}', [ 'v' => 1.1 ], '1.1' ],
+			[ '{v}', [ 'v' => null ], '<null>' ],
+			[ '{v}', [ 'v' => true ], '<true>' ],
+			[ '{v}', [ 'v' => false ], '<false>' ],
+			[ '{a} {b}', [ 'a' => '{b}', 'b' => 'c' ], '{b} c' ],
+		];
+	}
+
+}
